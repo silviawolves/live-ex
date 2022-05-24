9533ab4208133037bfdda47cbbccae1316d6a201
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 12:51:54 +0200

    tolto array - completo

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const numeriScelti = []
 let sumNumeri = 0
 
 for (let i = 0; i < 10; i++) {
     let numeriInseriti = prompt("Inserisci un numero")
     numeriRisultanti = parseInt(numeriInseriti)
     console.log(numeriRisultanti)
-    numeriScelti.push(numeriRisultanti)
     sumNumeri += numeriRisultanti
 }
 
