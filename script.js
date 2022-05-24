b37e729cc0df31a336c2554a210f9b526cd687fb
silviawolves <silviacanepa.w@gmail.com>
2022-05-24 12:50:42 +0200

    minore e non minore e uguale

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const numeriScelti = []
 let sumNumeri = 0
 
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i < 10; i++) {
     let numeriInseriti = prompt("Inserisci un numero")
     numeriRisultanti = parseInt(numeriInseriti)
     console.log(numeriRisultanti)
